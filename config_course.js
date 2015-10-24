f72b777cdadbcd40939306747e2a2475f6c9c17c
Vinay Gaba <vinaygaba@gmail.com>
2015-10-24 01:30:13 -0400

    Added Config_Course and Config_Student

---
+++ config_course.js
@@ -0,0 +1,26 @@
+var pg = require('pg');
+var connectionString = 'postgres://postgres:postgres@localhost:5432/infinity_course_db';
+//var redis = require('redis');
+var client = new pg.Client(connectionString);
+client.connect();
+
+
+
+
+exports.addColumn = function(req){	
+console.log('Method Called');
+
+var column_name = req.body.column_name;
+
+var column_type = req.body.column_name;
+
+
+
+var query = client.query("Alter Table ms_course_tbl Add Column " + column_name + " " + column_type);
+
+query.on('end', function(result) {
+console.log("Row successfully inserted");
+	//client.end();
+});
+
+}
